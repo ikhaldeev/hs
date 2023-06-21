af5c43693f9d26a15b34e363bf02d02c4218dfe6
Ivan Khaldeev <ikhaldeev@yandex.ru>
2023-06-21 02:04:44 +0200

    Include patients ns to tests

--- test/clj/hs/test_runner.clj
+++ test/clj/hs/test_runner.clj
@@ -1,10 +1,11 @@
 (ns hs.test-runner
   (:require
     [clojure.test :refer [run-tests successful?]]
-    [hs.handler-test]))
+    [hs.handler-test]
+    [hs.patients-test]))
 
 (defn run
   [_args]
-  (let [result (run-tests 'hs.handler-test)
+  (let [result (run-tests 'hs.handler-test 'hs.patients-test)
         exit-code (if (successful? result) 0 1)]
     (System/exit exit-code)))
